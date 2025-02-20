6cf7e8ae462e04a162ef698b6f0bf552a7328eaa
20212pf.cc0012 <20212pf.cc0012@pc-207512.passofundo.ifsul.local>
2025-02-20 09:30:01 -0300

    Bee1001 e Bee1003

---
+++ TOO-2025-1/src/BeeCrowd/Bee1001.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BeeCrowd;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 20212pf.cc0012
+ */
+public class Bee1001 {
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+        
+        int A = entrada.nextInt();
+        
+        int B = entrada.nextInt();
+        
+        int X = A + B;
+        
+        System.out.println("X = " + X);
+    }
+}
