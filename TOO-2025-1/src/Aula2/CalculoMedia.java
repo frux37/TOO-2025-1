3076491eb5c804e3ac8141f3407081980670dc9b
20212pf.cc0012 <20212pf.cc0012@pc-226004.passofundo.ifsul.local>
2025-02-26 10:12:44 -0300

    media

--- TOO-2025-1/src/Aula2/CalculoMedia.java
+++ TOO-2025-1/src/Aula2/CalculoMedia.java
@@ -3,13 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Aula2;
-
+import java.util.Scanner;
 /**
  *
  * @author 20212pf.cc0012
  */
 public class CalculoMedia {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Olá, Mundo!");
     }
 }
