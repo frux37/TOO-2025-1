7e4908804d8053f222a4069da7538392e8b62a55
20212pf.cc0012 <20212pf.cc0012@pc-226004.passofundo.ifsul.local>
2025-02-26 11:16:28 -0300

    Bee1158

--- TOO-2025-1/src/Aula2/CalculoMedia.java
+++ TOO-2025-1/src/Aula2/CalculoMedia.java
@@ -14,5 +14,32 @@ public class CalculoMedia {
         
         System.out.println("Informe o nome do aluno: ");
         String nome = entrada.nextLine();
+        
+        System.out.println("Informe a 1a nota do aluno "+ nome);
+        double n1 = entrada.nextInt();
+        
+        System.out.println("Informe a 2a nota do aluno "+ nome);
+        double n2 = entrada.nextInt();
+        
+        System.out.println("Informe a 3a nota do aluno "+ nome);
+        double n3 = entrada.nextInt();
+        
+        float media = (float)(n1+n2+n3) / 3; // Casting para conversão explicita
+        
+        System.out.println("A media do aluno " +nome+ ": " +media);
+        System.out.printf("A media do aluno %s: %.2f\n", nome, media);
+        
+        /*if(media >= 6) {
+            System.out.println("Aluno "+nome+ " foi aprovado");
+        } else if(media >= 5) {
+            System.out.println("Aluno "+nome+ " está em reprovado");
+        } else {
+            System.out.println("Aluno "+nome+ " está em reavaliacao");
+        }
+        */
+        
+        String resultado = media >= 6 ? "APROVADO" : "EM REAVALIACAO";
+        
+        System.out.println("Aluno(a)" +nome+ " "+resultado);
     }
 }
---
+++ TOO-2025-1/src/BeeCrowd/Bee1158.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BeeCrowd;
+
+import java.util.Scanner;
+/**
+ *
+ * @author 20212pf.cc0012
+ */
+public class Bee1158 {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Informe a quantidade de casos de teste");
+        int N = entrada.nextInt();
+        
+        System.out.println("Informe um valor inteiro");
+        String X = entrada.nextLine();
+        
+        //System.out.println("Informe outro valor inteiro:");
+        //int Y = entrada.nextInt();
+        
+        for(int i = 0; i < X; i++) {
+            
+        }
+        
+    }
+}
